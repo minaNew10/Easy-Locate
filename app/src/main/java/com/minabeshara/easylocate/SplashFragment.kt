acb3508ea3db7fe5367b6a503f3c5403a45f1c6e
minaNew10 <minadevdream@gmail.com>
2022-02-24 20:46:06 +0200

    add maps fragment

--- app/src/main/java/com/minabeshara/easylocate/SplashFragment.kt
+++ app/src/main/java/com/minabeshara/easylocate/SplashFragment.kt
@@ -1,15 +1,22 @@
 package com.minabeshara.easylocate
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 
 class SplashFragment : Fragment(R.layout.fragment_splash) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToMapsFragment())
+        }, 4000)
     }
 
 }
